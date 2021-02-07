bb8a04561614ed9b675c1f2c7b3585cf084790d9
Dobro <matematikaigor@gmail.com>
2021-02-07 16:00:57 +0300

    finish 2nd step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,18 +2,12 @@
 
 """Test."""
 
-import argparse
-import sys
+from gendiff import gendiff_cli
 
 
 def main():
-    """Algorithm."""
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-
-    args = parser.parse_args()
-    sys.stdout.write(args)
+    """CLI command."""
+    gendiff_cli.gendiff()
 
 
 if __name__ == '__main__':
